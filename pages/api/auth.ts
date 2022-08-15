19b848274c4ddf9e1048aa9e9ba613d20fafb4e9
BuildTools <unconfigured@null.spigotmc.org>
2022-08-14 20:55:39 -0500

    fix type errors

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,5 +1,5 @@
 import supabase from "../../supabase.config";
 
-export default function handler(req, res) {
+export default function handler(req: any, res: any) {
   supabase.auth.api.setAuthCookie(req, res);
 }
